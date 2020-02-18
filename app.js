bdd1ca290968d8cebda63e3aa7c39b4eb97c5fe4
Rémi Salmi <remi.salmi@gmail.com>
2020-02-18 08:45:34 +0100

    add categories routes

--- app.js
+++ app.js
@@ -13,15 +13,18 @@ app.listen(port, () => {
 const usersRoutes = require('./routes/usersRoutes');
 const remarksRoutes = require('./routes/remarksRoutes');
 const answersRoutes = require('./routes/answersRoutes');
+const categoriesRoutes = require('./routes/categoriesRoutes');
 
 app.use('/answers', answersRoutes);
 app.use('/remarks', remarksRoutes);
 app.use('/users', usersRoutes);
+app.use('/categories', categoriesRoutes);
 
 //Controllers imports
 const answersController = require('./controllers/answersController');
 const usersController = require('./controllers/usersController');
 const remarksController = require('./controllers/remarksController');
+const categoriesController = require('./controllers/categoriesController');
 
 
 
