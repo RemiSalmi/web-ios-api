938fb279bf5a2b38c0262e0e3c161accb5dfcc0c
Rémi <remi.salmi@gmail.com>
2020-03-18 21:14:36 +0100

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,15 +3,26 @@ const jwt = require('jsonwebtoken')
 const secret = require('../config/security')
 
 exports.isConnected = (req, res, next) => {
-    if (req.body.token) {
-        jwt.verify(req.body.token, secret, (err, token) => {
+    if(req.headers.authorization){
+        jwt.verify(req.headers.authorization, secret, (err, token) => {
             if (err) {
                 res.status(401).json({"message": "Error, not connected"})
             } else {
                 next()
             }
-        })
-    }else {
-        res.status(401).json({"message": "Error, not connected"})
+        }) 
+    }else{
+        if (req.body.token) {
+            jwt.verify(req.body.token, secret, (err, token) => {
+                if (err) {
+                    res.status(401).json({"message": "Error, not connected"})
+                } else {
+                    next()
+                }
+            })
+        }else {
+            res.status(401).json({"message": "Error, not connected"})
+        }
     }
+    
 }
